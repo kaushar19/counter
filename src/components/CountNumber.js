429594a64967a053d93aff1bc041c2f6507ae395
kaushar19 <kausharbtt@gmail.com>
2024-07-21 16:05:18 +0530

    first commit

--- src/components/CountNumber.js
+++ src/components/CountNumber.js
@@ -1,28 +1,37 @@
 import React, { useState } from "react";
-function CountNumber() {
+import "./CountNumber.css";
+function CountNumber({ text }) {
   const [count, setCount] = useState(0);
+
   function Increase(e) {
     setCount(count + 1);
   }
   function Decrease(e) {
     setCount(count - 1);
   }
+  var getTextStyle = () => {
+    if (count > 1) {
+      return { color: "green" };
+    } else if (count === 0) {
+      return { color: "black" };
+    } else {
+      return { color: "red" };
+    }
+  };
   return (
     <>
       <div className="flex-container">
         <div style={{ alignSelf: "center" }}>
-          <button className="btn ">
-            <span className="btntext" onClick={Decrease}>
-              Decrease
-            </span>
+          <button className="btn " onClick={Decrease}>
+            <span className="btntext">Decrease</span>
           </button>
         </div>
-        <div style={{ alignSelf: "center", fontSize: 30 }}>{count}</div>
+        <div className="counttextt textShow" style={getTextStyle()}>
+          {count}
+        </div>
         <div style={{ alignSelf: "center" }}>
-          <button className="btn ">
-            <span className="btntext" onClick={Increase}>
-              Increase
-            </span>
+          <button className="btn " onClick={Increase}>
+            <span className="btntext">Increase</span>
           </button>
         </div>
       </div>
